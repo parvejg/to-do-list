aff3a627d17aef24717bf19eb832c53e7e365625
parvejg <parvezpg321@gmail.com>
2023-12-02 15:02:47 +0530

    complete todos list work

--- src/Components/ListPage.js
+++ src/Components/ListPage.js
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import "./ListPage.css";
 export const ListPage = () => {
-  const [listInput, setListInput] = useState("");
+  const [newTodosText, SetNewTodosText] = useState("");
+  const [todo, setTodo] = useState([]);
+  const newTodosObj = {};
+  const addToListHandler = () => {
+    if (newTodosText) {
+      newTodosObj.text = newTodosText;
+      newTodosObj.id = todo?.length + 1;
+      newTodosObj.isTodoListComplete = false;
+    }
+    setTodo([...todo, newTodosObj]);
+    SetNewTodosText("");
+  };
   return (
     <div className="list-page-main-wrapper">
       <div className="input-main-wrapper">
@@ -10,15 +21,29 @@ export const ListPage = () => {
             type="text"
             className="list-input"
             placeholder="Title...."
-            onChange={(e) => setListInput(e.target.value)}
+            value={newTodosText}
+            onChange={(e) => SetNewTodosText(e.target.value)}
           />
-          <button className="list-add-btn">Add List</button>
+          <button className="list-add-btn" onClick={addToListHandler}>
+            Add List
+          </button>
         </div>
       </div>
       <div className="list-wrapper">
-        <li className="item-list">{listInput}</li>
-        <li className="item-list">hedfhdfhdshd</li>
-        <li className="item-list">hedfhdfhdshd</li>
+        {todo?.map((newList) => {
+          return (
+            <div key={newList.id} className="todos-list">
+              <li
+                value={newList.id}
+                className="item-list"
+                onClick={(e) => console.log(e.target.value)}
+              >
+                {newList.text}
+              </li>
+              <span className="close-btn">X</span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
